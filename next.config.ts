f9bae55a366e04a1dd3fdc6667d20382ae4abc21
Aditya Trivedi <adit4443ya@gmail.com>
2025-05-25 16:09:47 +0530

    update

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig = {
   pageExtensions: ["ts", "tsx", "mdx"],
   output: "export",
-  basePath: "/aditya-gsoc-blog",
-  assetPrefix: "/aditya-gsoc-blog/",
+  
 };
 export default nextConfig;
