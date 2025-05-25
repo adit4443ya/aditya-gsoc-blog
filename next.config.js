7bb8c653dfb4a46c73f980096d576a4d8c28785c
Aditya Trivedi <adit4443ya@gmail.com>
2025-05-25 14:56:58 +0530

    Fix basepath

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import("next").NextConfig} */
+const nextConfig = {
+  pageExtensions: ["ts", "tsx", "mdx"],
+  output: "export",
+  basePath: "/aditya-gsoc-blog",
+  assetPrefix: "/aditya-gsoc-blog/",
+};
+
+module.exports = nextConfig;
